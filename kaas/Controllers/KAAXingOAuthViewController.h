f0b1c88749799c576ff65e798c1dc23c66839e22
Girgis Ghattas <g.ghattas@defacto.nl>
2013-11-21 12:16:56 +0100

    Add empty XingOAuthViewController

---
+++ kaas/Controllers/KAAXingOAuthViewController.h
@@ -0,0 +1,13 @@
+//
+//  KAAXingOAuthViewController.h
+//  kaas
+//
+//  Created by Girgis on 21/11/13.
+//  Copyright (c) 2013 Jurre Stender. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KAAXingOAuthViewController : UIViewController
+
+@end
---
+++ kaas/Controllers/KAAXingOAuthViewController.m
@@ -0,0 +1,17 @@
+//
+//  KAAXingOAuthViewController.m
+//  kaas
+//
+//  Created by Girgis on 21/11/13.
+//  Copyright (c) 2013 Jurre Stender. All rights reserved.
+//
+
+#import "KAAXingOAuthViewController.h"
+
+@interface KAAXingOAuthViewController ()
+
+@end
+
+@implementation KAAXingOAuthViewController
+
+@end
