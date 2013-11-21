72f5e2e94eae42358e7dbca73d442379ed02574a
Girgis Ghattas <g.ghattas@defacto.nl>
2013-11-21 11:34:12 +0100

    Add User model

---
+++ kaas/Models/KAAUser.h
@@ -0,0 +1,17 @@
+//
+//  KAAUser.h
+//  kaas
+//
+//  Created by Girgis on 21/11/13.
+//  Copyright (c) 2013 Jurre Stender. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KAAUser : NSObject
+
+@property NSString *username;
+@property NSString *emailAddress;
+@property NSInteger userId;
+
+@end
