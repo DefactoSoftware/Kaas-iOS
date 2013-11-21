e699b6befc62877953987d16039ce3916fd80ff5
Girgis Ghattas <g.ghattas@defacto.nl>
2013-11-21 14:52:43 +0100

    Make basic XING OAuth connection

--- kaas/Controllers/KAAXingOAuthViewController.m
+++ kaas/Controllers/KAAXingOAuthViewController.m
@@ -7,11 +7,74 @@
 //
 
 #import "KAAXingOAuthViewController.h"
+#import <gtm-oauth/GTMOAuthAuthentication.h>
+#import <gtm-oauth/GTMOAuthViewControllerTouch.h>
 
-@interface KAAXingOAuthViewController ()
-
-@end
+static NSString *const KAAXingConsumerKey = @"6a875450a5af3a3ce6a9";
+static NSString *const KAAXingConsumerSecret = @"9a3057d7b93b7fed0520022f4ee8217391be4c72";
+static NSString *const KAAXingAPIBaseUrl = @"https://api.xing.com/v1/";
+static NSString *const KAAXingRequestEndpoint = @"request_token";
+static NSString *const KAAXingAccessEndpoint = @"access_token";
+static NSString *const KAAXingAuthorizeEndpoint = @"authorize";
+static NSString *const KAAOAuthCallbackUrl = @"http://kaas.defacto.nl/success";
+static NSString *const KAAXingKeychainItemName = @"xing";
 
 @implementation KAAXingOAuthViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self signInToXing];
+}
+
+- (GTMOAuthAuthentication *)xingAuthentication {
+    GTMOAuthAuthentication *authentication = [[GTMOAuthAuthentication alloc] initWithSignatureMethod:kGTMOAuthSignatureMethodHMAC_SHA1
+                                                                               consumerKey:KAAXingConsumerKey
+                                                                                privateKey:KAAXingConsumerSecret];
+    
+    [authentication setServiceProvider:@"XING"];
+    
+    return authentication;
+}
+
+- (void)signInToXing {
+    NSString *requestUrl = [NSString stringWithFormat:@"%@%@", KAAXingAPIBaseUrl, KAAXingRequestEndpoint];
+    NSString *accessUrl = [NSString stringWithFormat:@"%@%@", KAAXingAPIBaseUrl, KAAXingAccessEndpoint];
+    NSString *authorizeUrl = [NSString stringWithFormat:@"%@%@", KAAXingAPIBaseUrl, KAAXingAuthorizeEndpoint];
+    NSString *scope = @"";
+    
+    GTMOAuthAuthentication *authentication = [self xingAuthentication];
+
+    [authentication setCallback:KAAOAuthCallbackUrl];
+
+    GTMOAuthViewControllerTouch *viewController;
+    viewController = [[GTMOAuthViewControllerTouch alloc] initWithScope:scope
+                                                               language:nil
+                                                        requestTokenURL:[NSURL URLWithString:requestUrl]
+                                                      authorizeTokenURL:[NSURL URLWithString:authorizeUrl]
+                                                         accessTokenURL:[NSURL URLWithString:accessUrl]
+                                                         authentication:authentication
+                                                         appServiceName:KAAXingKeychainItemName
+                                                               delegate:self
+                                                       finishedSelector:@selector(viewController:finishedWithAuth:error:)];
+    
+    [[self navigationController] pushViewController:viewController animated:YES];
+}
+
+- (void)viewController:(UIViewController *)viewController
+        finishedWithAuth:(GTMOAuthAuthentication *)auth
+                   error:(NSError *)error {
+    if (error != nil) {
+        // sign in failed
+    } else {
+        // sign in succeeded
+        //
+        // With a GTMHTTPFetcher, use the auth object as an authorizer,
+        // like
+        //   [fetcher setAuthorizer:auth];
+        //
+        // or use it to sign a request directly, like
+        //    [auth authorizeRequest:myNSURLMutableRequest]
+    }
+}
 
 @end
